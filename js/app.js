aca8f157bb6ae7a15854788b262b4c7a8c3868e7
asif12018 <asif.request@gmail.com>
2024-02-13 23:41:05 +0600

    added

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 
 //starting home code:
 
+function continueGame(){
+   const alphabet = getARandomAlphabet();
+   const currentAlphabetElement = document.getElementById('current-alphabet');
+   currentAlphabetElement.innerText = alphabet;
+   
+
+   setBackgroundColorById(alphabet);
+
+
+}
+
 function play(){
     hideElementById('home')
     showElementById('play-ground');
+    continueGame();
 }
--- js/utility.js
+++ js/utility.js
@@ -5,3 +5,21 @@ function hideElementById(element){
 function showElementById(element){
     document.getElementById(element).classList.remove('hidden');
 }
+
+
+function setBackgroundColorById(elementbyId){
+    const element = document.getElementById(elementbyId);
+    element.classList.add('bg-orange-400');
+}
+
+
+function getARandomAlphabet(){
+    const alphabetString = 'qwertyuiopasdfghjklzxcvbnm/';
+    const alphabets = alphabetString.split('');
+    
+    const randomNumber = Math.random() * 26;
+    const index = Math.round(randomNumber);
+    const alphabet = alphabets[index];
+    return alphabet
+}
+
